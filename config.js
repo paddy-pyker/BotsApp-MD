5a36816eeca6e1cca4cc5b15e7859c003b4dc0e8
Peter Dorglo <paddypyker@gmail.com>
2022-01-27 10:54:04 +0000

    project files cleaned

--- config.js
+++
@@ -1,5 +0,0 @@
-var env = {
-    PREFIX: "^[.?!]",
-    CODE_CLIENT_ID: "3429fedd853367fe1e21651da075e403",
-    CODE_CLIENT_SECRET: "fb19833a244f42bf4fd0dccb9e78136db70724e1846491bca4647ee360b182d7"
-};
