c12a01d1e420324aaa1c26f4895abf2cf48498ad
Peter Dorglo <paddypyker@gmail.com>
2022-01-28 05:23:28 +0000

    weather module completed

--- config.ts
+++ config.ts
@@ -2,6 +2,7 @@ const env = {
     PREFIX: "^[.?!]" ,
     CODE_CLIENT_ID:"3429fedd853367fe1e21651da075e403",
     CODE_CLIENT_SECRET:"fb19833a244f42bf4fd0dccb9e78136db70724e1846491bca4647ee360b182d7",
-    TEMP_FILE_LOCATION:"/home/atsu/BotsApp-MD/"
+    TEMP_FILE_LOCATION:"/home/atsu/BotsApp-MD/",
+    WEATHER_API_KEY:"6729ac2b2e2bb5c686ff427a2f06df92"
 }
 module.exports = env
