a9797bf0a91595d044be89ba8ab3a09a2e2db515
Peter Dorglo <paddypyker@gmail.com>
2022-02-08 00:34:34 +0000

    book module updated

--- modules/book.ts
+++ modules/book.ts
@@ -53,8 +53,13 @@ module.exports = {
                         const book_metadata = $(`a[href=${currentvalue}]`)[0].parent.prev.prev
                         const type = book_metadata.children[0].data
                         const size = book_metadata.prev.prev.children[0].data
-                        //console.log(book_metadata.prev.prev.prev.prev.prev.prev.children[0].data)
-                        const pages = book_metadata.prev.prev.prev.prev.prev.prev.children[0].data
+                        let pages
+                        try {
+                            pages = book_metadata.prev.prev.prev.prev.prev.prev.children[0].data
+                        } catch (e) {
+                            pages = ""
+                        }
+
 
                         const book = {
                             url: undefined,
