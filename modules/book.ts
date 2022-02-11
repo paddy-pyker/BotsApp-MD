b1802f115cdb4a44b5a5f8901728ceecf93059a8
Peter Dorglo <paddypyker@gmail.com>
2022-02-11 20:57:59 +0000

    changed mimetype of document

--- modules/book.ts
+++ modules/book.ts
@@ -58,9 +58,11 @@ module.exports = {
 
             const file_name = book["name"].replace(/\W/g,'_') + "."+ book["extension"].toLowerCase()
 
+            const mimeType = book["extension"] === "PDF" ? "application/pdf" : "application/epub+zip"
+
             await  TRANSMIT.sendMessageWTyping(client,BotsApp.chat,{image:{url:book["cover"]},caption:caption,thumbnail:null})
 
-            await  TRANSMIT.sendMessageWTyping(client,BotsApp.chat,{document:{url:book["downloaded_book_location"]},fileName:file_name,pageCount:book["pages"]?book["pages"]:""}).catch(err => inputSanitization.handleError(err, client, BotsApp));
+            await  TRANSMIT.sendMessageWTyping(client,BotsApp.chat,{document:{url:book["downloaded_book_location"]},fileName:file_name,mimetype:mimeType,pageCount:book["pages"]?book["pages"]:""}).catch(err => inputSanitization.handleError(err, client, BotsApp));
 
             return await inputSanitization.deleteFiles(book["downloaded_book_location"])
 
