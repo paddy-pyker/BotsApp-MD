cf75aab67b7ab52fb460783db8fab4ddcdac4b5e
Peter Dorglo <paddypyker@gmail.com>
2022-02-11 22:11:44 +0000

    updated zlibrary

--- zlibrary/main.py
+++ zlibrary/main.py
@@ -1,12 +1,41 @@
 import sys
 import asyncio
+import random
 from libasync import AsyncZlib
 
 async def main():
 	
     lib = AsyncZlib()
 
-    await lib.login("mixoc50575@petloca.com", "12345678")
+    email_address_bank = [
+
+        "noliki5365@petloca.com",
+        "golava6358@goonby.com",
+        "kihif83549@plexfirm.com",
+        "cixose8227@petloca.com",
+        "xobewom869@plexfirm.com",
+        "ferixip802@plexfirm.com",
+        "somive9637@petloca.com",
+        "gogebe9672@petloca.com",
+        "vokehos377@petloca.com",
+        "tobef54361@diolang.com",
+        "tomah57102@plexfirm.com",
+        "gejofoh506@diolang.com",
+        "penebak706@plexfirm.com",
+        "xicev98180@plexfirm.com",
+        "rehip92573@diolang.com",
+        "lakaga6379@plexfirm.com",
+        "wodopif456@goonby.com",
+        "kimayi2712@goonby.com",
+        "walece6933@goonby.com",
+        "jalilec747@petloca.com"
+    ]
+
+    email_address = random.choice(email_address_bank)
+
+    password = "12345678"
+
+    await lib.login(email_address, password)
 
     await lib.init()
 
