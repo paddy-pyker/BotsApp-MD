37068543789435216f9a2bafd8b517980c79898d
Peter Dorglo <paddypyker@gmail.com>
2022-02-23 02:57:55 +0000

    auth file not downloaded again if exists

--- sidekick/getauth.ts
+++ sidekick/getauth.ts
@@ -1,10 +1,14 @@
 const axios = require('axios');
 const fss = require('fs')
 
-axios.get(process.env.URL, {responseType: "stream"} )
+if(!fss.existsSync( "auth_info_multi.json")){
+
+    axios.get(process.env.URL, {responseType: "stream"} )
     .then(response => {
         response.data.pipe(fss.createWriteStream("auth_info_multi.json"));
     })
     .catch(error => {
         console.log(error);
     });
+}
+
